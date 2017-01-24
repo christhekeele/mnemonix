68c716f3eaa03b66d660020c96001d53e64505ca
Chris Keele <dev@chriskeele.com>
2017-01-24 04:59:50 -0800

    Provide faster implementations of serialization for in-memory stores.

--- lib/mnemonix/stores/map.ex
+++ lib/mnemonix/stores/map.ex
@@ -36,6 +36,34 @@ defmodule Mnemonix.Stores.Map do
     {:ok, %{}}
   end
 
+  @doc false
+  @spec serialize_key(Mnemonix.key, Mnemonix.Store.t)
+    :: serialized_key :: term | no_return
+  def serialize_key(key, _store) do
+    key
+  end
+
+  @doc false
+  @spec serialize_value(Mnemonix.value, Mnemonix.Store.t)
+    :: serialized_value :: term | no_return
+  def serialize_value(value, _store) do
+    value
+  end
+
+  @doc false
+  @spec deserialize_key(serialized_key :: term, Mnemonix.Store.t)
+    :: Mnemonix.key :: term | no_return
+  def deserialize_key(serialized_key, _store) do
+    serialized_key
+  end
+
+  @doc false
+  @spec deserialize_value(serialized_value :: term, Mnemonix.Store.t)
+    :: Mnemonix.value :: term | no_return
+  def deserialize_value(serialized_value, _store) do
+    serialized_value
+  end
+
   @spec delete(Mnemonix.Store.t, Mnemonix.key)
     :: {:ok, Mnemonix.Store.t} | Mnemonix.Store.Behaviour.exception
   def delete(store = %Store{state: map}, key) do
