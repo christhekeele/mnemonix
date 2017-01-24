5bc88f93a8b13791300671c58fb1af842a984fa5
Chris Keele <dev@chriskeele.com>
2017-01-24 11:58:11 -0800

    Release v0.7.1.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mnemonix.Mixfile do
     name: "Mnemonix",
     app: :mnemonix,
 
-    version: "0.7.0",
+    version: "0.7.1",
     elixir: "~> 1.3",
 
     build_embedded: Mix.env == :prod,
