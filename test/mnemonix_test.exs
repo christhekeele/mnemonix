f1feeedccff06610cb58a598a1992316463640b9
Chris Keele <dev@chriskeele.com>
2017-03-15 20:58:18 -0700

    Play with build matrix and experiment with deprecated size.

--- test/mnemonix_test.exs
+++ test/mnemonix_test.exs
@@ -4,23 +4,14 @@ defmodule Mnemonix.Test do
   doctest Mnemonix
 
   test "implements (almost) all functions in Map" do
-    missing = [
-      # drop: 2,
-      # equal?: 2,
+    exceptions = [
       from_struct: 1,
-      # keys: 1,
       merge: 2,
       merge: 3,
-      # split: 2,
-      # take: 2,
-      # to_list: 1,
-      # values: 1,
+      size: 1,
     ]
-    missing = if :gt == System.version |> Version.parse! |> Version.compare(Version.parse!("1.4.0")) do
-      Enum.sort missing ++ [size: 1]
-    else; missing; end
 
-    assert Map.__info__(:functions) -- Mnemonix.__info__(:functions) == missing
+    assert Map.__info__(:functions) -- Mnemonix.__info__(:functions) == exceptions
   end
 
 end
